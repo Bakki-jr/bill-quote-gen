a70ffe98455f1739842440f20ae7a296217d349d
Bakki-jr <bakki.jr@gmail.com>
2023-05-19 20:55:53 +0530

    removed dark mode text for flavour checkbox text

--- app/page.js
+++ app/page.js
@@ -143,7 +143,7 @@ export default function Home() {
                   />
                   <label
                     htmlFor="green-checkbox"
-                    className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    className="ml-2 text-sm font-medium text-gray-900"
                   >
                     {info.flavour}
                   </label>
